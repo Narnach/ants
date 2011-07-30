a0105ee5fc378caec7922f5fcf9593a1fa5b9d58
Wes Oldenbeuving <narnach@gmail.com>
2011-07-30 17:14:16 +0200

    Bot tries N/E/S/W in a random order to reduce chance of getting stuck by making random moves

--- MyBot.rb
+++ MyBot.rb
@@ -1,5 +1,16 @@
 #!/usr/bin/env ruby
 
+class Array
+  def shuffle
+    shuffled = self.dup
+    self.size.times do |index|
+      replacement_index = rand(self.size)
+      shuffled[index], shuffled[replacement_index] = shuffled[replacement_index], shuffled[index]
+    end
+    shuffled
+  end
+end
+
 $:.unshift File.join(File.dirname(__FILE__), 'lib')
 require 'ai'
 
@@ -14,7 +25,7 @@ ai.run do |ai|
 	
 	ai.my_ants.each do |ant|
 		# try to go north, if possible; otherwise try east, south, west.
-		[:N, :E, :S, :W].each do |dir|
+		[:N, :E, :S, :W].shuffle.each do |dir|
 			if ant.square.neighbor(dir).free?
 				ant.order dir
 				break
